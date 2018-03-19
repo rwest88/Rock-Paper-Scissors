0a8de2ed73af4f8ec656d15a39679b6cf7014c4c
Ryan Weston <rwest88.va@gmail.com>
2018-03-19 18:03:01 -0400

    database records users and current players

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,68 +12,146 @@ var database = firebase.database();
 
 
 
+var player1 = null;
+var player2 = null;
+var player1Name = null;
+var player2Name = null;
+var userName;
+var displayName;
+var turn = 1;
 
-var player = {
-    userName: userName,
-    whichPlayer: null,
-    choice: null,
-    turn: null,
-    name: null,
-    wins: null,
-    losses: null,
-    globalWins: null,
-    globalLosses: null,
-    rock: null,
-    paper: null,
-    scissors: null,
-    batman: null,
-    ozzy: null,
-    lizard: null,
-    spock: null
-};
+var user;
 
-
-///////////////////////////////////
-var whichPlayer = 1;
-// if player 1 exists in database, whichPlayer = 2
-// else create player 1
 function whoseturnisit() {}
 function playerleaves() {}
 
-var userName;
-var logins = 0;
 
-function writePlayerData(player, key, value) {
-    var obj = [];
-    obj[key] = value;
-    database.ref('/players/' + player).set(obj);
-}
+//// first we need to listen for any connected users and store or delete our local objects
+database.ref("/players/").on("value", function(snapshot) {
+	// Check for existence of player 1 in the database
+	if (snapshot.child("player1").exists()) {
+		console.log("Player 1 exists");
 
-function playerExists() {
-    firebase.database().ref('/players/').once('value').then(function(snapshot) {
-        if (snapshot.child(userName).exists()) {
-            console.log("exists");
-            return true;
-        }
-    });
-}
-//once("value", snapshot => snapshot.forEach(child => console.log(child.val()))
-database.ref().on("value", function(snapshot) {
-    console.log(snapshot.val().logins);
-    logins = snapshot.val().logins;
+		// Record player1 data
+		player1 = snapshot.val().player1;
+		player1Name = player1.name;
+    } else {
+		console.log("Player 1 does NOT exist");
+
+		player1 = null;
+        player1Name = "";
+    }
+    if (snapshot.child("player2").exists()) {
+		console.log("Player 2 exists");
+
+		// Record player2 data
+		player2 = snapshot.val().player2;
+        player2Name = player2.name;
+    } else {
+		console.log("Player 2 does NOT exist");
+
+		player2 = null;
+		player2Name = "";
+    }
 });
+
+// Attach a listener to the database /turn/ node to listen for any changes
+
+//once("value", snapshot => snapshot.forEach(child => console.log(child.val()))
+
+// database.ref().on("value", function(snapshot) {
+//     console.log(snapshot.val().logins);
+//     logins = snapshot.val().logins;
+// });
 
 ////////////////////////////
 
 $('#submit').click(function() {
+
     event.preventDefault();
-    $('form').addClass("hide");
-    player.userName = $('#fname').val().trim() + " " + $('#lname').val().trim();
-    logins++;
-    //database.ref().set({logins: logins});
-    // database.ref("/players/" + userName).set(player);
-    writePlayerData(player.userName, 'peepee', 0);
-    writePlayerData(player.userName, 'poopoo', 23);
+
+    // if both players aren't queued...
+    if ( ($('#fname').val().trim() !== "" && $('#lname').val().trim() !== "") && !(player1 && player2) ) { 
+
+        // check if player 1 exists and if not, write to local object player1 and set it to the database
+        if (player1 == null) {
+            userName = $('#fname').val().trim() + " " + $('#lname').val().trim();
+            displayName = $('#fname').val().trim() + " " + $('#lname').val().trim().charAt(0);
+            player1 = {
+                name: userName,
+                displayName: displayName,
+                wins: 0,
+                losses: 0,
+                globalWins: 0,
+                globalLosses: 0,
+                choice: "",
+                rock: 0,
+                paper: 0,
+                scissors: 0,
+                batman: 0,
+                ozzy: 0,
+                lizard: 0,
+                spock: 0
+            };
+
+            // check if the user has played before and retrieve his/her data
+            database.ref('/users/').once('value').then(function(snapshot) {
+                if (snapshot.child(userName).exists()) {
+                    console.log("welcome back");
+                    console.log(snapshot.child(userName + "/globalWins").val());
+                    player1.globalWins = snapshot.child(userName + "/globalWins").val();
+                    player1.globalLosses = snapshot.child(userName + "/globalWins").val();
+                }
+                // set updated player stats to database
+                database.ref().child("/players/player1").set(player1);
+                database.ref().child("/users/" + userName).set(player1);
+                database.ref().child("/turn/").set(1);
+            });
+
+            // if user disconnects, remove player
+            database.ref("/players/player1").onDisconnect().remove();
+
+        // check if player 2 exists and if not, write to local object player2 and set it to the database
+        } else if (player2 == null) {
+            
+            userName = $('#fname').val().trim() + " " + $('#lname').val().trim();
+            displayName = $('#fname').val().trim() + " " + $('#lname').val().trim().charAt(0);
+            player2 = {
+                name: userName,
+                displayName: displayName,
+                wins: 0,
+                losses: 0,
+                globalWins: 0,
+                globalLosses: 0,
+                choice: "",
+                rock: 0,
+                paper: 0,
+                scissors: 0,
+                batman: 0,
+                ozzy: 0,
+                lizard: 0,
+                spock: 0
+            };
+
+            // check if the user has played before and retrieve his/her data
+            database.ref('/users/').once('value').then(function(snapshot) {
+                if (snapshot.child(userName).exists()) {
+                    console.log("welcome back");
+                    console.log(snapshot.child(userName + "/globalWins").val());
+                    player2.globalWins = snapshot.child(userName + "/globalWins").val();
+                    player2.globalLosses = snapshot.child(userName + "/globalWins").val();
+                }
+                // set updated player stats to database
+                database.ref().child("/players/player2").set(player2);
+                database.ref().child("/users/" + userName).set(player2);
+                database.ref().child("/turn/").set(1);
+            });
+
+            // if user disconnects, remove player
+            database.ref("/players/player2").onDisconnect().remove();
+        }
+        $('form').addClass("hide");
+    }
 
 });
 
