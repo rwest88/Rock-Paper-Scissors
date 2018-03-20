b157c999b545599af535d201c6c298405d55c762
Ryan Weston <rwest88.va@gmail.com>
2018-03-20 03:01:34 -0400

    testing mobile

---
+++ assets/javascript/oldapp.js
@@ -0,0 +1,74 @@
+    // if (userGuess == "r") {var userGuessLong = "rock";}
+    // if (userGuess == "p") {var userGuessLong = "paper";}
+    // if (userGuess == "s") {var userGuessLong = "scissors";}
+    // if (userGuess == "b") {var userGuessLong = "batman";}
+    // if (userGuess == "o") {var userGuessLong = "ozzy";}
+    // if (userGuess == "l") {var userGuessLong = "lizard";}
+    // if (userGuess == "k") {var userGuessLong = "spock";}
+
+    // // Randomly chooses a choice from the options array. This is the Computer's guess.
+    // var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // if (computerGuess == "r") {var computerGuessLong = "rock";}
+    // if (computerGuess == "p") {var computerGuessLong = "paper";}
+    // if (computerGuess == "s") {var computerGuessLong = "scissors";}
+    // if (computerGuess == "b") {var computerGuessLong = "batman";}
+    // if (computerGuess == "o") {var computerGuessLong = "ozzy";}
+    // if (computerGuess == "l") {var computerGuessLong = "lizard";}
+    // if (computerGuess == "k") {var computerGuessLong = "spock";}
+    
+    // // Alerts the key the user pressed (userGuess).
+    // alert("User guess: " + userGuessLong + ",  Computer guess: " + computerGuessLong);
+
+    // if (computerGuess == userGuess) {alert("Tie!"); ties++;}
+
+    // if (userGuess == "r" && computerGuess == "s") {alert("You Win! Rock smashes Scissors."); wins++;}
+    // if (userGuess == "r" && computerGuess == "b") {alert("You Win! Rock crushes Batman."); wins++;}
+    // if (userGuess == "r" && computerGuess == "l") {alert("You Win! Rock smashes Lizard."); wins++;}
+    // if (userGuess == "r" && computerGuess == "p") {alert("You Lose! Paper covers Rock."); losses++;}
+    // if (userGuess == "r" && computerGuess == "o") {alert("You Lose! Ozzy smokes Rock."); losses++;}
+    // if (userGuess == "r" && computerGuess == "k") {alert("You Lose! Spock vaporizes Rock."); losses++;}
+
+    // if (userGuess == "p" && computerGuess == "b") {alert("You Win! Paper (money) spoils Batman."); wins++;}
+    // if (userGuess == "p" && computerGuess == "k") {alert("You Win! Paper disproves Spock."); wins++;}
+    // if (userGuess == "p" && computerGuess == "r") {alert("You Win! Paper covers Rock."); wins++;} 
+    // if (userGuess == "p" && computerGuess == "o") {alert("You Lose! Ozzy smokes Paper."); losses++;} 
+    // if (userGuess == "p" && computerGuess == "s") {alert("You Lose! Scissors cuts Paper."); losses++;}
+    // if (userGuess == "p" && computerGuess == "l") {alert("You Lose! Lizard eats Paper."); losses++;}
+
+    // if (userGuess == "s" && computerGuess == "o") {alert("You Win! Scissors dehair Ozzy."); wins++;}
+    // if (userGuess == "s" && computerGuess == "l") {alert("You Win! Scissors decapitates Lizard."); wins++;}
+    // if (userGuess == "s" && computerGuess == "p") {alert("You Win! Scissors cuts paper."); wins++;} 
+    // if (userGuess == "s" && computerGuess == "r") {alert("You Lose! Rock smashes Scissors."); losses++;}  
+    // if (userGuess == "s" && computerGuess == "b") {alert("You Lose! Batman deflects Scissors."); losses++;}
+    // if (userGuess == "s" && computerGuess == "k") {alert("You Lose! Spock smashes Scissors."); losses++;}
+
+    // if (userGuess == "b" && computerGuess == "o") {alert("You Win! Batman decapitates (extracts revenge upon) Ozzy."); wins++;} 
+    // if (userGuess == "b" && computerGuess == "k") {alert("You Win! Batman outvoices Spock."); wins++;} 
+    // if (userGuess == "b" && computerGuess == "s") {alert("You Win! Batman deflects Scissors."); wins++;} 
+    // if (userGuess == "b" && computerGuess == "l") {alert("You Lose! Lizard poisons Batman."); losses++;}
+    // if (userGuess == "b" && computerGuess == "r") {alert("You Lose! Rock crushes Batman."); losses++;}
+    // if (userGuess == "b" && computerGuess == "p") {alert("You Lose! Paper (money) spoils Batman."); losses++;}
+
+    // if (userGuess == "o" && computerGuess == "l") {alert("You Win! Ozzy decapitates lizard."); wins++;}
+    // if (userGuess == "o" && computerGuess == "r") {alert("You Win! Ozzy smokes Rock."); wins++;}
+    // if (userGuess == "o" && computerGuess == "p") {alert("You Win! Ozzy smokes Paper."); wins++;} 
+    // if (userGuess == "o" && computerGuess == "k") {alert("You Lose! Spock outsmarts Ozzy."); losses++;} 
+    // if (userGuess == "o" && computerGuess == "s") {alert("You Lose! Scissors dehair Ozzy."); losses++;} 
+    // if (userGuess == "o" && computerGuess == "b") {alert("You Lose! Batman decapitates (extracts revenge upon) Ozzy."); losses++;} 
+
+    // if (userGuess == "l" && computerGuess == "p") {alert("You Win! Lizard eats Paper."); wins++;}
+    // if (userGuess == "l" && computerGuess == "b") {alert("You Win! Lizard poisons Batman."); wins++;} 
+    // if (userGuess == "l" && computerGuess == "k") {alert("You Win! Lizard poisons Spock."); wins++;}  
+    // if (userGuess == "l" && computerGuess == "r") {alert("You Lose! Rock smashes Lizard."); losses++;}
+    // if (userGuess == "l" && computerGuess == "s") {alert("You Lose! Scissors decapitate Lizard."); losses++;}
+    // if (userGuess == "l" && computerGuess == "o") {alert("You Lose! Ozzy decapitates Lizard."); losses++;} 
+
+    // if (userGuess == "k" && computerGuess == "r") {alert("You Win! Spock vaporizes Rock."); wins++;}
+    // if (userGuess == "k" && computerGuess == "s") {alert("You Win! Spock smashes Scissors."); wins++;} 
+    // if (userGuess == "k" && computerGuess == "o") {alert("You Win! Spock outsmarts Ozzy."); wins++;}  
+    // if (userGuess == "k" && computerGuess == "p") {alert("You Lose! Paper disproves Spock."); losses++;}
+    // if (userGuess == "k" && computerGuess == "b") {alert("You Lose! Batman outvoices Spock."); losses++;}
+    // if (userGuess == "k" && computerGuess == "l") {alert("You Lose! Lizard poisons Spock."); losses++;} 
+
+
+    // alert("Wins: " + wins + ",  Losses: " + losses + ",  Ties: " + ties);
